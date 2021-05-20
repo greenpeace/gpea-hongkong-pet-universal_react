e9ee21a315a511feeeae8250e5d9c6efdc28f4d9
KFC <chris.chan@greenpeace.org>
2021-05-20 11:18:35 +0800

    update path

--- mc-deploy.js
+++ mc-deploy.js
@@ -43,7 +43,7 @@ const EndpointURL = "https://cloud.greenhk.greenpeace.org/websign";
 const CampaignId = "7012u000000P4n7AAC";
 const interests = ["Health"]; // Arctic, Climate, Forest, Health, Oceans, Plastics
 const ftpConfigName = "ftp_hk"; // refer to ~/.npm-en-uploader-secret
-const ftpRemoteDir = "/2021/biodiversity";
+const ftpRemoteDir = "/2021/wallpaper";
 
 let indexHtmlFilePath = path.join(buildFolder, "index.html");
 let fbuf = fs.readFileSync(indexHtmlFilePath);
