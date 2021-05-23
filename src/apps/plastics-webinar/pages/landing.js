2756c6c0a2b40d99cd1141b1faa86e4b604acd0a
CK Lam <ckhk0621@gmail.com>
2021-05-23 16:04:28 +0800

    plastics-webinar init

---
+++ src/apps/plastics-webinar/pages/landing.js
@@ -0,0 +1,228 @@
+import "swiper/swiper.scss";
+import React from "react";
+import { connect } from "react-redux";
+import { useMediaQuery } from "react-responsive";
+import Sticky from "react-sticky-el";
+import { ChakraProvider, Box, Button, Divider, Image, Flex, Text, Heading, Link, SimpleGrid, Stack} from "@chakra-ui/react";
+import SEO from "../SEO";
+import content from "../data/content";
+import Nav from "../components/header/nav";
+import HeroSwiper from "../components/feature/heroSwiper";
+import Footer from "../components/footer";
+import NewFrameForm from "components/form/newFrameForm";
+import NewFrameSubmittedForm from "components/form/newFrameSubmittedForm";
+import Panel from "components/panel/newFormPanel";
+import * as themeActions from "store/actions/action-types/theme-actions";
+import themeConfig from "../theme.js";
+
+import banner from "../assets/images/PFC-webinar-banner-5.png";
+import subImage from "../assets/images/wallpaper_removed.jpg";
+
+const Landing = ({ submitted, togglePanel }) => {
+  const isMobile = useMediaQuery({ query: "(max-device-width: 564px)" });
+  const pStyle = {
+    as: "p",
+    marginTop: "20px",
+    color: "gray.900",
+    lineHeight: "1.7",
+    fontSize: "16px",
+  };
+
+  return (
+    <ChakraProvider theme={themeConfig}>
+      <SEO />
+      <Nav showButton={false} />
+      <Flex>
+        <Box flex="1" style={{ minWidth: "0px" }}>
+          <Box px={4} py={{ base: 4, md: 8 }} bgColor="#F9BC81">
+            <Box >
+              {!submitted && <Image src={banner} borderRadius="8px" />}
+              {submitted && !isMobile && (
+                <HeroSwiper isMobile={isMobile} swiperHeight="480px" />
+              )}{" "}
+              {/** Fixed swiper desktop version height, background image will keep ratio and center center position */}
+            </Box>
+
+
+            {/* <SimpleGrid mt="4" columns={{ base: 1, xl: 2 }} spacing={10}>
+              <Flex direction="column">
+                <Image
+                  borderRadius="8px"
+                  src={
+                    "https://www.greenpeace.org/static/planet4-hongkong-stateless/2021/05/0f48c407-gif_reg_ty_page.gif"
+                  }
+                />
+              </Flex>
+              <Stack spacing={4}>
+                <Heading
+                  as="h2"
+                  size="lg"
+                  color="brand.500"
+                  style={{ lineHeight: "1.5" }}
+                >
+                  一起守護
+                  <br />
+                  香港珍貴的自然資源！
+                </Heading>
+                <Text {...pStyle}>
+                  在高樓與繁忙都市之外，香港蘊含著豐富的生物多樣性，自然山野與遼闊海岸是眾多野生生物的居所。
+                </Text>
+                <Text {...pStyle}>
+                  請支持綠色和平以科學角度、調查研究及行動，奮力守護大嶼及保衛郊野，守護香港珍貴的自然資源，為下一代建設宜居的生活環境！
+                </Text>
+                <Link href="https://supporter.ea.greenpeace.org/hk/s/donate/donation-new?language=zh_HK&campaign=biodiversity&utm_campaign=biodiversity">
+                  <Button
+                    mt="2"
+                    color="#FFF"
+                    bg="brand.500"
+                    _hover={{ bg: "brand.400" }}
+                    borderRadius="4px"
+                    fontSize="md"
+                    type="button"
+                    letterSpacing={4}
+                  >
+                    立即捐助
+                  </Button>
+                </Link>
+                <Text {...pStyle}>
+                  綠色和平成立50年，從不接受政商界捐助，在環境工作上維持公正獨立！
+                </Text>
+              </Stack>
+            </SimpleGrid> */}
+          </Box>
+
+          {/** https://getwaves.io/ */}
+          <Box mt={-4}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 140"><path fill="#F9BC81" fill-opacity="1" d="M0,96L48,85.3C96,75,192,53,288,48C384,43,480,53,576,53.3C672,53,768,43,864,42.7C960,43,1056,53,1152,58.7C1248,64,1344,64,1392,64L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+          </Box>
+
+
+
+
+            <Box px={8}>
+            <Box>
+              <Text
+                as="h1"
+                fontSize={{ base: "2xl", sm: "3xl", xl: "4xl" }}
+                fontWeight="bold"
+                color="gray.900"
+                lineHeight={1.2}
+              >
+                <Text color="brand.500" mt={4} mb={2}>「綠色社區點做起？達人同你渾身解塑」網上共學教室</Text>
+              </Text>
+            </Box>
+            <Flex direction={{ base: "column", sm: "row" }}>
+                <Box flex="1">
+                  <Heading fontSize="2xl">覺得自己只是被動的一方？見到社會好多事都看似沒可能改變？</Heading>
+                  <Text as="p" {...pStyle}>
+              自己社區自己救！走塑不是一個人的事，是整個社區的事！我們的日常生活與社區密不可分，每人都是社區不可或缺的一分子。想自備餐盒買外賣？想走塑購物？總不能單靠個人力量，還需餐廳、企業的合作，一起建構走塑社區，讓大家日常生活都「有得揀」！
+              <br/><br/>
+              實現理想走塑社區這個終極目標好像很遙遠，不知如何開始第一步？綠色和平將於6月7日（星期一），舉辦網上共學教室「綠色社區點做起？達人同你渾身解塑」，邀請兩位積極連結街坊來改變社區的「社區達人」來分享：大埔街坊暨前區議員連桷璋，以及「日常豊作」裸買店店主李秀平Ann。談談過去的社區實驗，必定有成功有失敗，一起分享如何在社區推動議題，如何見招拆招！而要在社區推動走塑，嘉賓又有何想法？
+              <br/><br/>
+              有你，有可能！集齊腦袋，集思廣益！
+              <br/><br/>
+              是次共學教室只是頭炮活動！目標是召集各路人馬加入「全城走塑社區小隊」，一起討論、研究、分享、交流，一起設計社區走塑計劃，改變膠運，共建全城走塑的理想社區。想知道之後一連串的全城走塑社區小隊共學活動及行動？想跟綠色和平一起推動社區項目？6月7日，約定你！
+                  </Text>
+                </Box>
+              </Flex>
+            </Box>
+
+            <Divider my={{ base: 8, lg: 10 }} />
+
+
+
+
+
+        </Box>
+        <Box
+          w={{ base: 0, md: "50%", lg: "500px" }}
+          px="4"
+          py={{ base: 4, md: 8 }}
+          d={{ base: "none", md: "block" }}
+        >
+          <Sticky stickyStyle={{ zIndex: 10 }}>
+            <Box
+              borderTop="4px solid #66cc00"
+              boxShadow="lg"
+              p="6"
+              rounded="md"
+              bg="white"
+              overflow="hidden"
+            >
+              {submitted ? (
+                <NewFrameSubmittedForm formContent={content} />
+              ) : (
+                <NewFrameForm
+                  formContent={content}
+                  version={true}
+                  showProgress={false}
+                  newsLetter={false}
+                  birthDate={false}
+                  thanksScreen={true}
+                />
+              )}
+            </Box>
+          </Sticky>
+        </Box>
+      </Flex>
+      <Box
+        pos="fixed"
+        bottom={0}
+        zIndex={9}
+        p="4"
+        w="100%"
+        style={{
+          background: "rgba(255, 255, 255, 0.8)",
+          borderColor: "rgba(255, 255, 255, 0.8)",
+          boxShadow: "0px 0px 20px 0px rgb(0 0 0 / 10%)",
+          paddingTop: "6px",
+          paddingBottom: "8px",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        d={{ base: "flex", md: "none" }}
+      >
+        <Button
+          w="80%"
+          color="#FFF"
+          bg="orange"
+          borderRadius="24px"
+          fontSize="xl"
+          letterSpacing={4}
+          style={{ zIndex: 999 }}
+          onClick={() => togglePanel(true)}
+        >
+          {content.submit_text}
+        </Button>
+      </Box>
+      <Panel
+        formContent={content}
+        showProgress={false}
+        newsLetter={false}
+        birthDate={false}
+      >
+        {submitted && isMobile && (
+          <HeroSwiper isMobile={isMobile} swiperHeight="480px" />
+        )}
+      </Panel>
+      <Footer />
+    </ChakraProvider>
+  );
+};
+
+const mapStateToProps = ({ theme }) => {
+  return {
+    submitted: theme.lastAction === themeActions.SUBMIT_FORM_SUCCESS,
+    // submitted: false, // TEST submitted
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    togglePanel: (bol) => {
+      dispatch({ type: themeActions.TOGGLE_PANEL, bol });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
