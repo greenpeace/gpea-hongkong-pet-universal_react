c8719df90117867af76c93377c8c32862b4c6a01
CK Lam <ckhk0621@gmail.com>
2021-05-24 04:32:48 +0800

    updated

--- src/apps/plastics-webinar/pages/landing.js
+++ src/apps/plastics-webinar/pages/landing.js
@@ -51,14 +51,14 @@ const Landing = ({ submitted, togglePanel }) => {
     pos: "relative", 
     w: {base: '50px', sm: '90px'}, 
     h: {base: '50px', sm: '90px'}, 
-    border: "1px solid #FFF",
+    border: "2px solid #FFF",
     pb: 10
   }
 
   const switchButtonWrap = {
     bgColor: "#FFF", 
     roundedTop: 'md',
-    px: {base: 4, sm: 12}, 
+    px: {base: 4, sm: 14}, 
     pt:{base: 2, sm: 4}, 
     pb:{base: 2, sm: 4},
     boxShadow: 'xl',
@@ -127,14 +127,14 @@ const Landing = ({ submitted, togglePanel }) => {
                             />
                           }
                           iconBg={useColorModeValue("yellow.100", "yellow.900")}
-                          text={"日期：2021年4月24日（星期六）"}
+                          text={"日期：2021年6月7日（星期一）"}
                         />
                         <Feature
                           icon={
                             <Icon as={IoTimeSharp} color={"yellow.500"} w={5} h={5} />
                           }
                           iconBg={useColorModeValue("yellow.100", "yellow.900")}
-                          text={"時間：下午4時半至5時半"}
+                          text={"時間：晚上8時至9時"}
                         />
                         <Feature
                           icon={
@@ -165,10 +165,10 @@ const Landing = ({ submitted, togglePanel }) => {
               </Flex>
 
 
-              <Box mx={-8} pt={10}>
+              <Box pt={10}>
                 <Flex direction="column">
                   <Center>
-                  <Stack direction="row" spacing={{base: 0, sm: 12}} px={{base: 4, sm: 8}}>
+                  <Stack direction="row" spacing={{base: 0, sm: 4}} px={{base: 4, sm: 8}}>
                   {authorContent.map((d,i)=><Box
                   onClick={()=> setCurrent(d)}  
                   {...switchButtonWrap}
@@ -184,7 +184,7 @@ const Landing = ({ submitted, togglePanel }) => {
                     </Box>)}
                   </Stack>
                   </Center>
-                  <Box bgColor="#F9BC81" py={8} px={{base: 6, sm: 24}}>
+                  <Box bgColor="#F9BC81" py={8} px={{base: 6, sm: 16}} borderRadius={8}>
                     <Box color="#FFF" py={{base: 4, sm: 12}}>
                       <Heading fontSize={'2xl'} mb={4}>{current.title}</Heading>
                       <Text as="p" fontSize={'xl'}>
@@ -199,53 +199,55 @@ const Landing = ({ submitted, togglePanel }) => {
 
             </Box>
 
-            {/* <Divider my={{ base: 8, lg: 10 }} />
+            <Box px={{base: 4, sm: 8}} pb={12}>
+            <Divider my={{ base: 8, lg: 10 }} />
 
-            <SimpleGrid mt="4" columns={{ base: 1, xl: 2 }} spacing={10}>
-              <Flex direction="column">
-                <Image
-                  borderRadius="8px"
-                  src={
-                    "https://www.greenpeace.org/static/planet4-hongkong-stateless/2021/05/0f48c407-gif_reg_ty_page.gif"
-                  }
-                />
-              </Flex>
-              <Stack spacing={4}>
-                <Heading
-                  as="h2"
-                  size="lg"
-                  color="brand.500"
-                  style={{ lineHeight: "1.5" }}
-                >
-                  一起守護
-                  <br />
-                  香港珍貴的自然資源！
-                </Heading>
-                <Text {...pStyle}>
-                  在高樓與繁忙都市之外，香港蘊含著豐富的生物多樣性，自然山野與遼闊海岸是眾多野生生物的居所。
-                </Text>
-                <Text {...pStyle}>
-                  請支持綠色和平以科學角度、調查研究及行動，奮力守護大嶼及保衛郊野，守護香港珍貴的自然資源，為下一代建設宜居的生活環境！
-                </Text>
-                <Link href="https://supporter.ea.greenpeace.org/hk/s/donate/donation-new?language=zh_HK&campaign=biodiversity&utm_campaign=biodiversity">
-                  <Button
-                    mt="2"
-                    color="#FFF"
-                    bg="brand.500"
-                    _hover={{ bg: "brand.400" }}
-                    borderRadius="4px"
-                    fontSize="md"
-                    type="button"
-                    letterSpacing={4}
-                  >
-                    立即捐助
-                  </Button>
-                </Link>
-                <Text {...pStyle}>
-                  綠色和平成立50年，從不接受政商界捐助，在環境工作上維持公正獨立！
-                </Text>
-              </Stack>
-            </SimpleGrid> */}
+<SimpleGrid mt="4" columns={{ base: 1, xl: 2 }} spacing={10}>
+  <Flex direction="column">
+    <Image
+      borderRadius="8px"
+      src={
+        "https://www.greenpeace.org/static/planet4-hongkong-stateless/2021/05/0f48c407-gif_reg_ty_page.gif"
+      }
+    />
+  </Flex>
+  <Stack spacing={4}>
+    <Heading
+      as="h2"
+      size="lg"
+      color="brand.500"
+      style={{ lineHeight: "1.5" }}
+    >
+      一起守護
+      <br />
+      香港珍貴的自然資源！
+    </Heading>
+    <Text {...pStyle}>
+      在高樓與繁忙都市之外，香港蘊含著豐富的生物多樣性，自然山野與遼闊海岸是眾多野生生物的居所。
+    </Text>
+    <Text {...pStyle}>
+      請支持綠色和平以科學角度、調查研究及行動，奮力守護大嶼及保衛郊野，守護香港珍貴的自然資源，為下一代建設宜居的生活環境！
+    </Text>
+    <Link href="https://supporter.ea.greenpeace.org/hk/s/donate/donation-new?language=zh_HK&campaign=biodiversity&utm_campaign=biodiversity">
+      <Button
+        mt="2"
+        color="#FFF"
+        bg="brand.500"
+        _hover={{ bg: "brand.400" }}
+        borderRadius="4px"
+        fontSize="md"
+        type="button"
+        letterSpacing={4}
+      >
+        立即捐助
+      </Button>
+    </Link>
+    <Text {...pStyle}>
+      綠色和平成立50年，從不接受政商界捐助，在環境工作上維持公正獨立！
+    </Text>
+  </Stack>
+</SimpleGrid>
+            </Box>
 
         </Box>
 
