481caae152c2c4a56acda35ea7f799b8e7d058ac
KFC <chris.chan@greenpeace.org>
2021-06-03 10:59:12 +0800

    Update textStyles.js

--- src/styles/components/textStyles.js
+++ src/styles/components/textStyles.js
@@ -7,30 +7,33 @@ export const TextStyles = {
   variants: {
     heading: {
       color: "brand.500",
-      fontSize: {base: "2xl", sm: "3xl"},
-      fontWeight: 700
+      fontSize: { base: "2xl", sm: "3xl" },
+      fontWeight: 700,
     },
     paragraph: {
       color: "gray.900",
-      fontSize: {base: "sm", sm: "18px"},
-      fontWeight: 500,
-      lineHeight: '30px'
+      fontSize: { base: "sm", sm: "md" },
+      lineHeight: "1.5",
+    },
+    caption: {
+      color: "gray.700",
+      fontSize: { base: "sm" },
+      lineHeight: "1.5",
     },
     authorName: {
       color: "brand.500",
-      fontSize: {base: "2xl", sm: "3xl"},
-      fontWeight: 700
+      fontSize: { base: "2xl", sm: "3xl" },
+      fontWeight: 700,
     },
     authorTitle: {
       color: "gray.500",
-      fontSize: {base: "sm", sm: "md"},
-      fontWeight: 500,
-      lineHeight: '24px'
-    }
+      fontSize: { base: "md" },
+      lineHeight: "1.2",
+    },
   },
   // default values for `size` and `variant`
   defaultProps: {},
-}
+};
 
 export const HeadingStyles = {
   // style object for base or default style
@@ -38,8 +41,7 @@ export const HeadingStyles = {
   // styles for different sizes ("sm", "md", "lg")
   sizes: {},
   // styles for different visual variants ("outline", "solid")
-  variants: {
-  },
+  variants: {},
   // default values for `size` and `variant`
   defaultProps: {},
-}
+};
