326ae2c4a07cd9ff8b710da3d7bffc41fcf449a8
CK Lam <ckhk0621@gmail.com>
2021-06-03 09:43:40 +0800

    minor change

--- src/apps/wallpaper/index.js
+++ src/apps/wallpaper/index.js
@@ -47,7 +47,7 @@ const Index = ({ submitted, activeABTesting, setVariant, togglePanel }) => {
     }
   }, [submitted]);
 
-  return !submittedForm ? <Download /> : <Landing />;
+  return submittedForm ? <Download /> : <Landing />;
 };
 
 const mapStateToProps = ({ theme }) => {
