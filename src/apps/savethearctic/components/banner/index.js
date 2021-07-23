76d285258e2d159f6580bf9621f88e3af9723f68
KFC <chris.chan@greenpeace.org>
2021-07-23 18:24:06 +0800

    merge

--- src/apps/savethearctic/components/banner/index.js
+++ src/apps/savethearctic/components/banner/index.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from "react";
-import * as swiperActions from "store/actions/action-types/swiper-actions";
-import * as themeActions from "store/actions/action-types/theme-actions";
+import React, { useState, useEffect } from 'react'
+import * as swiperActions from 'store/actions/action-types/swiper-actions'
+import * as themeActions from 'store/actions/action-types/theme-actions'
 import SwiperCore, {
   Navigation,
   Mousewheel,
   Scrollbar,
   A11y,
   Autoplay,
-} from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { connect } from "react-redux";
+} from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { connect } from 'react-redux'
 
-SwiperCore.use([Scrollbar, A11y, Autoplay, Mousewheel]);
+SwiperCore.use([Scrollbar, A11y, Autoplay, Mousewheel])
 
 const Index = ({ swiperData, updateSwiperSlide, slideIndex, toggleTheme }) => {
-  const [swiper, setSwiper] = useState(null);
+  const [swiper, setSwiper] = useState(null)
 
   useEffect(() => {
     if (!swiper) {
-      return;
+      return
     }
 
-    swiper.slideTo(slideIndex, 1000);
-  }, [slideIndex]);
+    swiper.slideTo(slideIndex, 1000)
+  }, [slideIndex])
 
   useEffect(() => {
     const mySwiper = document.querySelector(
-      ".hero-slider-wrap_halftwo  .swiper-container"
-    ).swiper;
-    setSwiper(mySwiper);
-  }, []);
+      '.hero-slider-wrap_halftwo  .swiper-container'
+    ).swiper
+    setSwiper(mySwiper)
+  }, [])
 
   return (
     <Swiper
@@ -39,64 +39,64 @@ const Index = ({ swiperData, updateSwiperSlide, slideIndex, toggleTheme }) => {
       onSlideChange={(swiper) => updateSwiperSlide(swiper.activeIndex)}
     >
       {(swiperData || []).map((d) => {
-        const { header, title, description, cta } = d.content;
+        const { header, title, description, cta } = d.content
         return (
           <SwiperSlide key={d.id}>
-            <div className="swiper-container">
-              <div className="swiper-wrapper">
-                <div className="swiper-slide">
-                  <div className="half-hero-wrap">
+            <div className='swiper-container'>
+              <div className='swiper-wrapper'>
+                <div className='swiper-slide'>
+                  <div className='half-hero-wrap'>
                     <div>
-                      {header && <div className="hhw_header">{header}</div>}
+                      {header && <div className='hhw_header'>{header}</div>}
                       {title && (
                         <h1
-                          className="hhw_title"
+                          className='hhw_title'
                           dangerouslySetInnerHTML={{ __html: title }}
                         ></h1>
                       )}
                       {description && (
                         <div
-                          className="hhw_description"
+                          className='hhw_description'
                           dangerouslySetInnerHTML={{ __html: description }}
                         ></div>
                       )}
                       {cta.link && (
                         <button
-                          className="btn fl-btn color-bg"
+                          className='btn fl-btn color-bg'
                           onClick={() => toggleTheme(true)}
                         >
                           <span>{cta.content}</span>
                         </button>
                       )}
-                      <div className="clearfix"></div>
+                      <div className='clearfix'></div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </SwiperSlide>
-        );
+        )
       })}
     </Swiper>
-  );
-};
+  )
+}
 
 const mapStateToProps = ({ swiper }) => {
   return {
     swiperData: swiper.data,
     slideIndex: swiper.slideIndex,
-  };
-};
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
     updateSwiperSlide: (data) => {
-      dispatch({ type: swiperActions.UPDATE_SWIPER_SLIDE, data });
+      dispatch({ type: swiperActions.UPDATE_SWIPER_SLIDE, data })
     },
     toggleTheme: (bol) => {
-      dispatch({ type: themeActions.TOGGLE_FORM, bol });
+      dispatch({ type: themeActions.TOGGLE_FORM, bol })
     },
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Index);
+export default connect(mapStateToProps, mapDispatchToProps)(Index)
