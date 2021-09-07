c32aa30d366c7d134ebde8b52d20292a0aca1b59
KFC <chris.chan@greenpeace.org>
2021-09-07 16:55:51 +0800

    update app vars

--- mc-deploy.js
+++ mc-deploy.js
@@ -40,10 +40,10 @@ const buildFolder = path.join(__dirname, 'build');
 //
 // New websign endpoint can accept optional fields
 const EndpointURL = 'https://cloud.greenhk.greenpeace.org/websign';
-const CampaignId = '7012u000000P7vrAAC';
-const interests = ['Climate']; // Arctic, Climate, Forest, Health, Oceans, Plastics
+const CampaignId = '7012u000000Otu3AAC';
+const interests = ['Oceans']; // Arctic, Climate, Forest, Health, Oceans, Plastics
 const ftpConfigName = 'ftp_hk'; // refer to ~/.npm-en-uploader-secret
-const ftpRemoteDir = '/2021/climate-challenge';
+const ftpRemoteDir = '/2021/elm_content_b';
 
 let indexHtmlFilePath = path.join(buildFolder, 'index.html');
 let fbuf = fs.readFileSync(indexHtmlFilePath);
