d356d28e2f464356eb563c61c6974e22e7b50beb
KFC <chris.chan@greenpeace.org>
2021-09-20 10:59:23 +0800

    launch check

--- src/apps/forests/pages/content.js
+++ src/apps/forests/pages/content.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import {
-  Box,
-  Button,
-  Image,
-  Text,
-  Heading,
-  SimpleGrid,
-  Stack,
-} from '@chakra-ui/react';
+import { Box, Text, Heading, Stack } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 import ImageCarousel from 'components/banner/imageCarousel';
@@ -17,10 +9,19 @@ import kv from '../assets/images/GP1SU5JN_Medium_res.jpg';
 import img0 from '../assets/images/GP0STUG5D_Medium_res.jpg';
 import img1 from '../assets/images/GP1STT80_Medium_res.jpg';
 
-import banner from '../assets/images/GP1SU5AH_Medium_res.jpg';
-import banner2 from '../assets/images/GP0STU04Y_Medium_res.jpg';
+import animal1 from '../assets/images/GP0STU04Y_Medium_res.jpg';
+import animal2 from '../assets/images/GP1SUEHO_High_res.jpg';
+import animal3 from '../assets/images/GP04SB0_High_res.jpg';
+import animal4 from '../assets/images/GP04SF8_High_res.jpg';
 
-const SwiperData = [kv, img0, img1];
+import banner1 from '../assets/images/forest_petition.jpg';
+import banner2 from '../assets/images/argen.jpg';
+import banner3 from '../assets/images/amazon.jpg';
+import banner4 from '../assets/images/indonesia.jpg';
+
+const SwiperData = [banner1, banner2, banner3, banner4];
+
+const AnimalData = [animal1, animal2, animal3, animal4];
 
 const TextWrapper = styled.div`
   padding: 20px;
@@ -57,64 +58,27 @@ const Content = () => {
           </Text>
           <Text>
             但不法分子、無良企業非法砍伐與毁林，加上氣候危機加劇森林大火，令
-            <b>
+            <strong>
               全球每2秒就有相約1個足球場面積的森林消失、僅餘 15%森林被完整保護。
-            </b>
+            </strong>
           </Text>
           <Text>
             綠色和平正發起全球守護森林行動，要求各國政府訂立與加強護林政策、制止企業毁林行為，為森林提供復原空間。
           </Text>
         </TextWrapper>
 
-        <ParallaxBackground image={banner2} />
+        <ImageCarousel swiperData={AnimalData} />
 
         <TextWrapper>
-          <Heading as='h3' fontSize={'2xl'}>
+          <Heading as='h3' fontSize={'3xl'}>
             守護動物們的關鍵棲息地
           </Heading>
           <Text>
-            聯合國報告指出：全球八百多萬物種中，就有一百多萬面臨滅絕危機，包含美洲豹、蘇門答臘犀牛、紅毛猩猩等...
+            聯合國報告指出：
+            <strong>全球八百多萬物種中，就有一百多萬面臨滅絕危機</strong>
+            ，包含美洲豹、蘇門答臘犀牛、紅毛猩猩等...
           </Text>
         </TextWrapper>
-
-        <ParallaxBackground image={banner} />
-
-        {/* <TextWrapper>
-          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={6}>
-            <Image borderRadius='4px' src={banner2} />
-            <Stack spacing={4}>
-              <Heading as='h3' fontSize='2xl' style={{ lineHeight: '1.2' }}>
-                世上僅餘 15%
-                <br />
-                森林完好無損
-              </Heading>
-              <Text>
-                熱帶森林、溫帶雨林和北方森林，調節氣候、提供淡水和氧氣，更是地球約三分之二陸生動植物的棲息地，
-              </Text>
-              <Text>但因人類破壞令森林大火四起，並以驚人的速度消失...</Text>
-              <Text>
-                您的捐助，幫助綠色和平督促全球政府加強森林保護政策，要求企業終止砍伐、焚燒等毀林行為，採用生態友善且永續的方式管理森林。
-                過去50年，綠色和平堅持不接受政商界捐助，以公正獨立的身份守護全球森林，因為您，綠色和平守護環境的使命得以延續！
-              </Text>
-              <Button
-                mt='2'
-                color='#fff'
-                bg='orange.500'
-                _hover={{ textDecoration: 'none' }}
-                borderRadius='4px'
-                fontSize='md'
-                letterSpacing={2}
-                onClick={() =>
-                  window.open(
-                    'https://supporter.ea.greenpeace.org/hk/s/donate/donation-new?language=zh_HK&campaign=climate&ref=climate-challenge-thankyou'
-                  )
-                }
-              >
-                立即捐助
-              </Button>
-            </Stack>
-          </SimpleGrid>
-        </TextWrapper> */}
       </Stack>
     </Box>
   );
