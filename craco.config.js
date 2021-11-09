5cb1e8d39e42d691f4088cd9b03101f729a56895
KFC <chris.chan@greenpeace.org>
2021-11-09 11:47:29 +0800

    update packages

--- craco.config.js
+++ craco.config.js
@@ -1,31 +1,8 @@
-const CracoLessPlugin = require("craco-less");
-const CompressionPlugin = require("compression-webpack-plugin"); //gzip
-const BrotliPlugin = require("brotli-webpack-plugin"); //brotli
-const imageOptimizer = require("craco-image-optimizer-plugin");
+const CracoLessPlugin = require('craco-less'); //gzip
+const imageOptimizer = require('craco-image-optimizer-plugin');
 
 module.exports = {
   plugins: [
-    {
-      plugin: CompressionPlugin,
-      options: {
-        //gzip plugin
-        filename: "[path].gz[query]",
-        algorithm: "gzip",
-        test: /\.(js|css|html|svg)$/,
-        threshold: 8192,
-        minRatio: 0.8,
-      },
-    },
-    {
-      plugin: BrotliPlugin,
-      options: {
-        //brotli plugin
-        asset: "[path].br[query]",
-        test: /\.(js|css|html|svg)$/,
-        threshold: 10240,
-        minRatio: 0.8,
-      },
-    },
     {
       plugin: CracoLessPlugin,
       options: {
